184097a7526b7f20d6428a414089fb7c04abd80a
lucamilne <lucamilne@googlemail.com>
2023-09-12 20:11:26 +0100

    breakthrough

--- src/components/TheGrid.vue
+++ src/components/TheGrid.vue
@@ -1,18 +1,32 @@
 <template>
-  <ul class="grid grid-cols-3 gap-4" ref="dragThis">
-    <li>THIS</li>
-  </ul>
+  <div class="grid grid-cols-3 gap-4">
+    <div v-for="(value, key) in cells" :key="key" class="border aspect-square border-gray-800" ref="gridCells">
+      <div class="card block m-4 aspect-square bg-pink-300">{{ key }}</div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { Draggable } from '@shopify/draggable';
-import { ref, onMounted } from 'vue'
-const list = [1, 2, 3, 4, 5, 6, 7, 8];
-const dragThis = ref(null);
+import { ref, onMounted } from 'vue';
+const gridCells = ref([]);
+
+const cells = {
+  A1: {},
+  A2: {},
+  A3: {},
+  B1: {},
+  B2: {},
+  B3: {},
+  C1: {},
+  C2: {},
+  C3: {},
+}
 
 onMounted(() => {
-  const draggable = new Draggable(dragThis.value, {
-    draggable: 'li'
+  console.log(gridCells.value)
+  const draggable = new Draggable(gridCells.value, {
+    draggable: '.card'
   });
 })
 </script>
