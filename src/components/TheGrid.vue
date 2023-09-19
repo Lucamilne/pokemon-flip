04274f868941efe058a4e59fbc7999f4bc6ed61d
lucamilne <lucamilne@googlemail.com>
2023-09-19 20:23:47 +0100

    card manipulation #1

--- src/components/TheGrid.vue
+++ src/components/TheGrid.vue
@@ -1,8 +1,9 @@
 <template>
   <section>
+    {{ cellBeingDragged }}
     <div class="grid grid-cols-3" ref="gridCells">
       <div v-for="(value, key) in cells" :key="key" :class="`dropzone border border-4 border-zinc-400 aspect-square ${value.class}`">
-        <Card v-if="value.hasValue" />
+        <Card v-if="value.hasValue" :id="key" />
       </div>
     </div>
   </section>
@@ -14,43 +15,44 @@ import { ref, onMounted } from 'vue';
 import Card from './Card.vue'
 
 const gridCells = ref([]);
+const cellBeingDragged = ref(null);
 
 const cells = {
   A1: {
     hasValue: true,
-    class: 'border-r-0 border-b-0'
+    class: 'border-r-0 border-b-0',
   },
   A2: {
     hasValue: true,
-    class: 'border-r-0 border-b-0'
+    class: 'border-r-0 border-b-0',
   },
   A3: {
     hasValue: true,
-    class: 'border-b-0'
+    class: 'border-b-0',
   },
   B1: {
     hasValue: true,
-    class: 'border-b-0 border-r-0'
+    class: 'border-b-0 border-r-0',
   },
   B2: {
     hasValue: false,
-    class: 'border-b-0 border-r-0'
+    class: 'border-b-0 border-r-0',
   },
   B3: {
     hasValue: false,
-    class: 'border-b-0'
+    class: 'border-b-0',
   },
   C1: {
     hasValue: false,
-    class: 'border-r-0'
+    class: 'border-r-0',
   },
   C2: {
     hasValue: false,
-    class: 'border-r-0'
+    class: 'border-r-0',
   },
   C3: {
     hasValue: false,
-    class: ''
+    class: '',
   },
 }
 
@@ -63,11 +65,19 @@ onMounted(() => {
     },
   });
 
-  // let droppableOrigin;
+  droppable.on('drag:start', (event) => {
+    //
+  });
 
-  // droppable.on('drag:start', (event) => {
-  //   droppableOrigin = event.originalSource.parentNode;
-  // });
+  droppable.on('drag:stop', (event) => {
+    //
+  });
+
+  // manipulate mirror of dragged card
+  droppable.on('mirror:create', (event) => {
+    const mirror = event.data.source;
+    mirror.classList.add('z-50'); 
+  });
 
   droppable.on('droppable:dropped', (event) => {
     if (event.dropzone.children.length > 0) {
@@ -75,6 +85,10 @@ onMounted(() => {
     }
   });
 })
-
-
 </script>
+
+<style scoped>
+.z-index-100 {
+  z-index: 100 !important;
+}
+</style>
