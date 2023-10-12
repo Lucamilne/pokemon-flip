178520f00a0a37386a678713149634b610401efb
lucamilne <lucamilne@googlemail.com>
2023-10-12 19:56:39 +0100

    desktop loading screen complete

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,14 +1,16 @@
 <template>
-    <section class="absolute top-0 left-0 w-full h-full flex">
-        <div class="left basis-1/2 h-full bg-theme-red"></div>
-        <div class="basis-1/2 h-full flex relative">
+    <section class="absolute top-0 left-0 w-full h-full flex overflow-x-hidden">
+        <div class="pokeball-left basis-1/2 h-full bg-theme-red" :class="{ 'open-left': isOpen }"></div>
+
+
+        <div class="pokeball-right basis-1/2 h-full flex relative" :class="{ 'open-right': isOpen }">
             <div class="bg-black w-36 h-full">
             </div>
-            <div class="right w-full h-full bg-white">
+            <div class="pokeball-bottom w-full h-full bg-white">
             </div>
-            <div class="bg-white plinth rounded-full">
-                <button>
-                
+            <div class="bg-white surround rounded-full flex justify-center items-center" :class="{ 'fade-out': isOpen }">
+                <button class="pokeball-button bg-white rounded-full" @click.stop="isOpen = true">
+
                 </button>
             </div>
         </div>
@@ -22,27 +24,42 @@ export default {
         Loading: Boolean
     },
     data: () => ({
-        //
+        isOpen: false
     }),
 
 }
 </script>
 
 <style scoped>
-.left {
-    box-shadow: inset -32px 0px #901a22;
-    /* background: linear-gradient(to right, 
-    #901a22    50%, 
-      #e61919 50%,
-      #e61919 100%
-  ); */
+.pokeball-left {
+    box-shadow: inset -1.5rem 0px #901a22;
 }
 
-.right {
-    box-shadow: inset 32px 0px #d4d4d8;
+.pokeball-bottom {
+    box-shadow: inset 1.5rem 0px #d4d4d8;
 }
 
-.plinth {
+.pokeball-left,
+.pokeball-right {
+    transform: translateX(0%);
+    transition: transform 400ms ease;
+}
+
+.open-left {
+    transform: translateX(-100%);
+}
+
+.open-right {
+    transform: translateX(100%);
+}
+
+.fade-out {
+    transition: opacity 250ms ease;
+    transition-delay: 400ms;
+    opacity: 0;
+}
+
+.surround {
     position: absolute;
     top: 50%;
     transform: translateY(-50%);
@@ -50,5 +67,14 @@ export default {
     border: 2rem solid black;
     width: 18rem;
     height: 18rem;
+    box-shadow: inset 0 0 0 0.5rem #fff;
+    background: linear-gradient(to bottom, #d4d4d8 0%, #d4d4d8 50%, #a1a1aa 50%);
+}
+
+.pokeball-button {
+    width: 10rem;
+    height: 10rem;
+    border: 0.5rem solid black;
+    box-shadow: inset -0.5rem -0.5rem 0 0 #d4d4d8;
 }
 </style>
